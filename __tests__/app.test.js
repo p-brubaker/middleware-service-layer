146cda0a1e9e684ce2de1bcfe1a8a829c799ed7a
Paul Brubaker <pbrubak@gmail.com>
2021-09-26 21:27:10 -0700

    failing test for get comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,28 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+describe('middleware service layer', () => {
+    beforeEach(async () => {
+        await setup(pool);
+        const comments = await Promise.all([
+            { id: '1', comment: 'puppies are great!', toxic: false },
+            { id: '2', comment: 'your a poophead!', toxic: true },
+        ]);
+        await comments.forEach((comment) => {
+            request(app).post('api/v1/comments').send(comment);
+        });
+    });
 
-  afterAll(() => {
-    pool.end();
-  });
+    it('should retrieve a comment from the database', async () => {
+        const res = await request(app).get('api/v1/comments/1');
+        expect(res).toEqual({
+            id: '1',
+            comment: 'puppies are great!',
+            toxic: false,
+        });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
 });
